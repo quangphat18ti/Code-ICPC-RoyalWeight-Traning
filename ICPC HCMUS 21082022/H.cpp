f0196748cacf96c433a68b99abc8f3b1af39a98a
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2022-08-26 11:35:09 +0700

    Update H.cpp

--- ICPC HCMUS 21082022/H.cpp
+++ ICPC HCMUS 21082022/H.cpp
@@ -1,133 +1,202 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define db double
-#define nhat "H"
+
+#define all(_x) _x.begin(), _x.end()
+#define fi first
+#define se second
+#define sz(_s) int(s.size())
 #define pb push_back
-const int mm=2e5+5;
-const int oo=1e9+7;
-int n,m,k;
-struct ii
-{
-    int v,w;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+typedef vector<pii> vii;
+
+const int MAXN = 1e5 + 5;
+const int INF = 1e9 + 5;
+
+int n, m;
+vector<pii> adj[MAXN];
+vector<int> dist_1, dist_n;
+vector<int> min_1, min_n;
+vector<int> trace_min_1, trace_min_n; 
+vector<int> max_1, max_n;
+vector<int> trace_max_1, trace_max_n; 
+
+struct Edge{
+    int u, v, w;
+    void print() {
+        cout << "Edge(" << u << " -> " << v << ") = " << w << endl; 
+    }
 };
-vector<ii> e[mm],en[2][mm];
-void nhap()
-{
-    cin>>n>>m;
-    for(int i=1;i<=m;i++)
-    {
-        int x,y,z; cin>>x>>y>>z;
-        e[x].pb({y,z});
-        e[y].pb({x,z});
+vector<Edge> edges;
+
+ostream& operator << (ostream& out, vector<int> &st) {
+    out << st.size() - 1 << endl;
+    for(int u : st) out << u << ' '; cout << endl;
+    return out;
+}
+
+void build_adj(vector<Edge> &edges) {
+    for(int i = 1; i <= n; i++) adj[i].clear();
+
+    for(Edge tam : edges) {
+        adj[tam.u].push_back({tam.v, tam.w});
     }
 }
 
-int dd[mm],dd1[mm];
-queue<int> q;
-void bfs(int s)
-{
-    for(int i=1;i<=n;i++) dd[i]=oo;
-    dd[s]=0;
-    q.push(s);
-    while(q.size())
-    {
-        int u=q.front(); q.pop();
-        for(ii c:e[u])
-            if(dd[c.v]>dd[u]+1)
-                dd[c.v]=dd[u]+1,q.push(c.v);
+void init_data() {
+    cin >> n >> m;
+
+    edges.clear();
+    for(int i = 1; i <= m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        edges.push_back({u, v, w});
+        edges.push_back({v, u, w});
     }
-}
-void ddnn_1_n()
-{
-    bfs(1);
-    for(int i=1;i<=n;i++) dd1[i]=dd[i];
-    bfs(n);
-    k=dd1[n];
-}
-void build()
-{
-    for(int i=1;i<=n;i++)
-        for(ii j:e[i])
-            if(dd1[i]+1+dd[j.v]==k)
-                en[0][i].pb(j),en[1][j.v].pb({i,j.w});
+
+    build_adj(edges);
 }
 
-struct val
-{
-    int mi,tvmi;
-    int ma,tvma;
-};val st[2][mm];
-int kt[2][mm];
-void loang(int s,int id)
-{
-    q.push(s); kt[id][s]=1;
-    while(q.size())
-    {
-        int u=q.front(); q.pop();
-        for(ii nx:en[id][u])
-        {
-            if(!kt[id][nx.v]) kt[id][nx.v]=1,q.push(nx.v);
-            if(st[id][nx.v].ma<max(st[id][u].ma,nx.w))
-                st[id][nx.v].tvma=u,st[id][nx.v].ma=max(st[id][u].ma,nx.w);
-            if(st[id][nx.v].mi>min(st[id][u].mi,nx.w))
-                st[id][nx.v].tvmi=u,st[id][nx.v].mi=min(st[id][u].mi,nx.w);
+void bfs(int start, vector<int>& dist) {
+    dist.assign(n + 1, -1);
+    dist[start] = 0;
+
+    queue<int> que;
+    que.push(start);
+    while(que.size()){
+        int u = que.front(); que.pop();
+        for(pii edge : adj[u]) {
+            int v = edge.first;
+            if(dist[v] == -1) {
+                dist[v] = dist[u] + 1;
+                que.push(v);
+            }
         }
     }
 }
-void tim_min_max_1_n()
-{
-    for(int i=1;i<=n;i++)
-        for(int j=0;j<2;j++)
-            st[j][i]={oo,-oo},kt[j][i]=0;
-    loang(1,0);
-    loang(n,1);
+
+vector<Edge> build_another_graph(vector<Edge> edges, vi &dist_start,vi &dist_end, int end) {
+    vector<Edge> ans;
+    for(Edge &e : edges){
+        if(dist_start[e.u] + dist_end[e.v] + 1 == dist_start[end]) ans.push_back(e);
+    }
+    return ans;
 }
 
-int kq[mm];
-void xuli()
-{
-    ddnn_1_n();
-    build();
-    tim_min_max_1_n();
-    int re=-oo,jj,cc=0;
-    for(int i=2;i<n;i++)
-    {
-        if(re<st[0][i].ma-st[1][i].mi)
-            re=st[0][i].ma-st[1][i].mi,jj=i;
-        if(re<st[1][i].ma-st[0][i].mi)
-            re=st[1][i].ma-st[0][i].mi,jj=i,cc=1;
-    }
-    if(k==1)
-    {
-        cout<<"1\n1 "<<n;
-        return;
-    }
-    cout<<k<<endl;
-    if(!cc)
-    {
-        int tam=jj,r=0;
-        while(tam!=1) kq[++r]=tam,tam=st[0][tam].tvma;
-        kq[++r]=tam;
-        for(int i=r;i>=1;i--) cout<<kq[i]<<' ';
-        while(jj!=n) jj=st[1][jj].tvmi,cout<<jj<<' ';
-    }
-    else
-    {
-        int tam=jj,r=0;
-        while(tam!=1) kq[++r]=tam,tam=st[0][tam].tvmi;
-        kq[++r]=tam;
-        for(int i=r;i>=1;i--) cout<<kq[i]<<' ';
-        while(jj!=n) jj=st[1][jj].tvma,cout<<jj<<' ';
+void get_min_edge(int start, vector<int> &f, vector<int> &trace) {
+    f.assign(n + 1, INF);
+    trace.assign(n + 1, -1);
+
+    set<pii> min_set;
+    min_set.insert({f[start], start});
+
+    while(min_set.size()) {
+        pii tam = *min_set.begin();
+        min_set.erase(min_set.begin());
+        int u = tam.second;
+        if(tam.first != f[u]) continue;
+
+        for(pii nxt : adj[u]) {
+            int v = nxt.first;
+            int weight = nxt.second;
+
+            if(min(weight, f[u]) < f[v]) {
+                trace[v] = u;
+                f[v] = min(weight, f[u]);
+                min_set.insert({f[v], v});
+            }
+        } 
     }
 }
-int main()
-{
-//    freopen(nhat".inp","r",stdin);
-//    freopen(nhat".out","w",stdout);
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-    int t; t=1; while(t--){
-    nhap();
-    xuli();}
+
+void get_max_edge(int start, vector<int> &f, vector<int> &trace) {
+       f.assign(n + 1, -INF);
+       trace.assign(n + 1, - 1);
+       auto cmp = [](pii a, pii b) {return a.first > b.first;};
+       set<pii, decltype(cmp)> max_set(cmp);
+
+       max_set.insert({f[start], start});
+       while(max_set.size()) {
+            pii tam = *max_set.begin();
+            max_set.erase(max_set.begin());
+
+            int u = tam.second;
+            if(tam.fi != f[u]) continue;
+
+            for(pii nxt : adj[u]) {
+                int v = nxt.first;
+                int weight = nxt.second;
+
+                if(f[v] < max(f[u], weight)) {
+                    trace[v] = u;
+                    f[v] = max(f[u], weight);
+                    max_set.insert({f[v], v});
+                }
+            }
+       }
 }
+
+vector<int> truyvet(int u, vector<int> &trace){
+    vector<int> ans;
+    while(u!=-1){
+        ans.push_back(u);
+        u = trace[u];
+    }
+    return ans;
+}
+
+void concat(vector<int> &a, vector<int> b) {
+    for(int &u : b) a.push_back(u);
+}
+
+void solve() {
+    vector<Edge> temp_edges = build_another_graph(edges, dist_1, dist_n, n);
+    build_adj(temp_edges);
+    get_min_edge(1, min_1, trace_min_1);
+    get_max_edge(1, max_1, trace_max_1);
+
+
+    temp_edges = build_another_graph(edges, dist_n, dist_1, 1);
+    build_adj(temp_edges);
+    get_min_edge(n, min_n, trace_min_n);
+    get_max_edge(n, max_n, trace_max_n);
+
+    int ans = -INF; 
+    for(int i = 1; i <= n; i++) {
+        ans = max(ans, max_n[i] - min_1[i]);
+        ans = max(ans, max_1[i] - min_n[i]);
+    }
+
+    for(int i = 1; i <= n; i++) {
+       if(max_n[i] - min_1[i] == ans) {
+            vector<int> arr = truyvet(i, trace_min_1);
+            reverse(all(arr)); arr.pop_back();
+            concat(arr, truyvet(i, trace_max_n));   
+            cout << arr << endl;
+            return;
+       } 
+       
+       if(max_1[i] - min_n[i] == ans) {
+            vector<int> arr = truyvet(i, trace_max_1);
+            reverse(all(arr)); arr.pop_back();
+            concat(arr, truyvet(i, trace_min_n));
+            return;
+       }
+    }
+}
+
+int main() {
+//   freopen("input.txt", "r", stdin);
+//   freopen("output.txt", "w", stdout);
+    int t = 1;
+    //cin >> t;
+    while(t--) {
+        init_data();
+        bfs(1, dist_1);
+        bfs(n, dist_n);
+        solve();
+    }
+}
+
