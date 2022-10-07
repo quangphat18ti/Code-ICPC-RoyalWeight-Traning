a2b98b37d2447946d4506526b4221ccf1ea66e9b
quangphat18ti <70308391+quangphat18ti@users.noreply.github.com>
2022-10-07 10:39:04 +0700

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+
+#define fi first 
+#define se second
+
+#define fu(i, a, b) for(int i = int(a); i <= int(b); ++i)
+#define fd(i, a, b) for(int i = int(a); i >= int(b); --i)
+
+#define set(s, k) (s | (1LL << k));
+#define off(s, k) (s & ~(1LL << k));
+#define __builtin_popcount __builtin_popcountll
+ 
+
+typedef pair<ll, ll> pi;
+typedef vector<ll> vi;
+typedef vector<pi> vii;
+typedef vector<vector<ll>> vvi;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+
+}
